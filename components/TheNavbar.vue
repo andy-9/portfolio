1b3f40ac812ca095b052f5ccd8cb5795158b5262
Andreas Hechler <hechler@jpberlin.de>
2020-06-01 00:40:48 +0200

    links on navbar have more padding --> more user-friendly

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,47 +1,57 @@
 <template>
     <nav id="navbar">
         <div class="">
-            <nuxt-link class="" :to="{ name: 'index' }">Home</nuxt-link>
+            <nuxt-link class="nav-link" :to="{ name: 'index' }">Home</nuxt-link>
         </div>
+
         <div class="">
             <nuxt-link
-                class=""
+                class="nav-link"
                 :to="{ name: 'software' }"
                 title="Bigger and smaller projects I have built as a software engineer"
                 >Software</nuxt-link
             >
         </div>
+
         <div class="">
             <nuxt-link
-                class=""
+                class="nav-link"
                 :to="{ name: 'coaching' }"
                 title="Coaching, education, evaluation and other things I do"
                 >Coaching</nuxt-link
             >
         </div>
+
         <div class="">
             <nuxt-link
-                class=""
+                class="nav-link"
                 :to="{ name: 'publications' }"
                 title="Writing and speaking"
                 >Publications</nuxt-link
             >
         </div>
+
         <div class="">
-            <nuxt-link class="" :to="{ name: 'about' }" title="About me"
+            <nuxt-link class="nav-link" :to="{ name: 'about' }" title="About me"
                 >About</nuxt-link
             >
         </div>
+
         <div class="">
             <nuxt-link
-                class=""
+                class="nav-link"
                 :to="{ name: 'contact' }"
                 title="Get in touch, imprint & credits"
                 >Contact</nuxt-link
             >
         </div>
+
         <div class="">
-            <nuxt-link class="" :to="{ name: '' }" title="Not working yet">
+            <nuxt-link
+                class="nav-link-left"
+                :to="{ name: '' }"
+                title="Not working yet"
+            >
                 EN</nuxt-link
             >
             |
@@ -67,5 +77,13 @@ nav {
     display: flex;
     justify-content: space-around;
     align-items: center;
+}
+
+.nav-link {
+    padding: 0 2em;
+}
+
+.nav-link-left {
+    padding-left: 2em;
 }
 </style>
