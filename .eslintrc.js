14b1e3b07216880afe0a62558d72c8ea4485c265
Andreas Hechler <hechler@jpberlin.de>
2020-07-13 20:54:14 +0200

    change eslint on master

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {
-    // indent: ['error', 4]
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'prettier',
+        'prettier/vue',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: ['prettier'],
+    // add your custom rules here
+    rules: {
+        // indent: ['error', 4]
+        'max-lines': ['error', 150],
+        'max-lines-per-function': ['error', 20],
+        'max-len': ['off', { code: 130, tabWidth: 2 }],
+        'max-statements': ['error', 10],
+        'max-statements-per-line': ['error', { max: 2 }],
+        'max-params': ['error', 3],
+        'max-nested-callbacks': ['error', 4],
+        'max-depth': ['error', 2],
+        complexity: ['error', 5]
+    }
 }
